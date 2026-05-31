0827b21244f2691fd72bcc66e4862d93e3e13415
BoudiHamed <bodynody1@gmail.com>
2026-05-31 20:30:33 +0300

    add public folder for prototype Front-end section

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+import express from "express";
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
