dfab594015ee2eace8d751f40d83af41ffb94d37
BoudiHamed <bodynody1@gmail.com>
2026-06-01 13:31:53 +0300

    Database test schema

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
-
-
-const app = express();
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+// --------------------------
 const PORT = process.env.PORT || 5000;
+// --------------------------
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// -------------------------
+const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
