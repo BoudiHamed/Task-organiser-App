0104c7b65e7d445da95ede65f0188b1d08f23392
BoudiHamed <bodynody1@gmail.com>
2026-06-04 19:20:22 +0300

    crud operations

--- src/db.js
+++ src/db.js
@@ -1,21 +1,23 @@
-import { DatabaseSync } from "node:sqlite";
-const db = new DatabaseSync(":memory:");
+import { DatabaseSync } from 'node:sqlite'
+const db = new DatabaseSync(':memory:')
 
+// Execute SQL statements from strings
+db.exec(`
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+`)
 
-// create table if not exists
-db.exec(`CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT  UNIQUE NOT NULL,
-    password TEXT NOT NULL
-)`);
+db.exec(`
+    CREATE TABLE todos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        task TEXT,
+        completed BOOLEAN DEFAULT 0,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    )    
+`)
 
-
-db.exec(`CREATE TABLE todos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    task TEXT NOT NULL,
-    completed BOOLEAN NOT NULL DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-)`);
-
-export default db;
+export default db
--- src/server.js
+++ src/server.js
@@ -1,22 +1,35 @@
-import express from "express";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-// --------------------------
-const PORT = process.env.PORT || 5000;
-// --------------------------
+import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
+import authMiddleware from './middleware/authMiddleware.js'
+import dotenv from 'dotenv'
+dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-// -------------------------
-const app = express();
+const app = express()
+const PORT = process.env.PORT || 5003
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url)
+// Get the directory name from the file path
+const __dirname = dirname(__filename)
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// Middleware
+app.use(express.json())
+// Serves the HTML file from the /public directory
+// Tells express to serve all files from the public folder as static assets / file. Any requests for the css files will be resolved to the public directory.
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Serving up the HTML file from the /public directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+// Routes
+app.use('/auth', authRoutes)
+app.use('/todos', authMiddleware, todoRoutes)
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server has started on port: ${PORT}`)
+})
